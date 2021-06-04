91de01311e45b3ec7190f26d07def36ce2cab46f
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2021-06-03 19:34:56 -0500

    updated load_fact with implementation

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -6,16 +6,33 @@ from airflow.utils.decorators import apply_defaults
 class LoadFactOperator(BaseOperator):
     ui_color = '#F98866'
 
+    EXECUTE_STATEMENT: str = "INSERT INTO {table} {query};"
+
     @apply_defaults
     def __init__(
             self,
-            conn_id: str = "",
+            redshift_table: str = "",
+            query: str = "",
+            redshift_conn_id: str = "",
             *args,
             **kwargs
     ):
         super(LoadFactOperator, self).__init__(*args, **kwargs)
 
-        self.conn_id = conn_id
+        self._redshift_table = redshift_table
+        self._query = query
+        self._redshift_conn_id = redshift_conn_id
 
-    def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+    def execute(self, context: dict):
+        self.log.info(
+            f"Loading fact '{self._redshift_table}' table"
+        )
+
+        PostgresHook(
+            postgres_conn_id=self._redshift_conn_id
+        ).run(
+            self.EXECUTE_STATEMENT.format(
+                table=self._redshift_table,
+                query=self._query
+            )
+        )
